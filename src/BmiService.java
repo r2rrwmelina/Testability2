fb82b114af34c6b31abe5f3e9ea5e784483feb19
MG <gmelina69@gmail.com>
2026-02-17 21:13:58 +0400

    Update task 2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(double weightKg, double heightM) { // weight - кг, height - метр
-        int bmi = (int) (weightKg / (heightM * heightM));
+        int bmi = (int) (weightKg / Math.pow((heightM), 2));
         return bmi;
     }
 }
